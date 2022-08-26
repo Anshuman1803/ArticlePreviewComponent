6304926e4a4eb13c454f03fa682d86bead693e14
Anshuman Kumar <mahianshuman491@gmail.com>
2022-08-26 09:31:29 +0530

    Add files via upload

---
+++ BasicJavaScript.js
@@ -0,0 +1,11 @@
+// console.log("I am back");
+// let share= document.getElementById("ShareButton");
+// share.addEventListener("click", ()=>{
+//     console.log("You Click on the share button.")
+//     let popup = document.getElementById("myPopup");
+//     popup.classList.toggle("show");
+// })
+function PopUp(){
+    let popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
